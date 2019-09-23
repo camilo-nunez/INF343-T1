1df8d9993aa40f13be18c25fc27a0514e02cd73c
root <root@mx>
2019-09-22 21:38:51 -0300

    Local network and connection succesfull

--- Actividad 1/cliente.py
+++ Actividad 1/cliente.py
@@ -1,28 +1,23 @@
-# Python TCP Client A
-import socket 
+import socket
 import logging
 
-logging.basicConfig(level=logging.INFO, filename='respuestas.txt', filemode='w', format='%(asctime)s - %(message)s')
+logging.basicConfig(level = logging.INFO, filename = 'respuestas.txt', filemode = 'w', format = '%(asctime)s - %(message)s')
 
-#host = socket.gethostname()
-host = socket.gethostbyname(socket.gethostname())
+# obtiene IP propia
+host = "172.20.0.10"
 port = 5000
-BUFFER_SIZE = 2000 
-tcpClientA = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-tcpClientA.connect((host, port))
+BUFFER_SIZE = 1024
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Manda el mensaje de solicitud al servidor
-tcpClientA.send(b"Esta es la peticion !")
+cliente.connect( (host, port) )
 
-# Recive la confirmacion
-data = tcpClientA.recv(BUFFER_SIZE)
+# manda el mensaje de solicitud al servidor
+cliente.send(b"Esta es la peticion !")
+
+# recibe la confirmacion
+data = cliente.recv(BUFFER_SIZE)
 logging.info(data)
 print(data)
 
-# # Manda el mensaje 1
-# tcpClientA.send(b"Mensaje 1")
-
-# # Manda el mensaje 2
-# tcpClientA.send(b"Mensaje 2")
-
-tcpClientA.close()
+# cierre de socket
+cliente.close()
--- Actividad 1/servidor.py
+++ Actividad 1/servidor.py
@@ -3,62 +3,74 @@ from threading import Thread
 from socketserver import ThreadingMixIn
 import logging
 
-logging.basicConfig(level=logging.INFO, filename='log.txt', filemode='w', format='%(asctime)s - %(message)s')
+logging.basicConfig(level = logging.INFO, filename = 'log.txt', filemode = 'w', format = '%(asctime)s - %(message)s')
 
-# Multithreaded Python server : TCP Server Socket Thread Pool
-class ClientThread(Thread): 
+# ServerThread: servidor con PoolThread para multiples clientes
+class ServerThread(Thread): 
  
-    def __init__(self,ip,port): 
+    def __init__(self, ip, port):
+		
         Thread.__init__(self) 
         self.ip = ip 
         self.port = port 
-        logging.info("[+] New server socket thread started for " + ip + ":" + str(port))
-        print ("[+] New server socket thread started for " + ip + ":" + str(port))
+        info = "Se crea ServerThread para cliente con " + ip + ":" + str(port)
+        logging.info(info)
+        print(info)
  
     def run(self):
         
-        # Recive la conexion
-        data = conn.recv(2048).decode("utf-8")
+        # Recibe la conexion
+        data = conn.recv(1024).decode("utf-8")
         logging.info(data)
-        print (data)
+        print("Mensaje recibido: " + data)
 
         # Confirma la conexion
         conn.send('Confirmo la conexion')
 
-		# # Recive mensaje 1
-        # data = conn.recv(2048).decode("utf-8")
+		# # Recibe mensaje 1
+        # data = conn.recv(1024).decode("utf-8")
         # logging.info(data)
         # print (data)
 
 		# # Recive mensaje
-        # data = conn.recv(2048).decode("utf-8")
+        # data = conn.recv(1024).decode("utf-8")
         # logging.info(data)
         # print (data)
         
-        logging.info("[+] Close conection client.")
-        print("[+] Close conection client.")
+        logging.info("Cerrando conexion.")
+        print("Cerrando conexion.")
 
 
-# Multithreaded Python server : TCP Server Socket Program Stub
-TCP_IP = socket.gethostbyname(socket.gethostname()) 
-TCP_PORT = 5000
-BUFFER_SIZE = 20  # Usually 1024, but we need quick response 
+# Servidor multithread
 
+IP = socket.gethostbyname(socket.gethostname()) 
+PORT = 5000
+BUFFER_SIZE = 1024  # cambiar en caso de que se quiera
 
-tcpServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-tcpServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
-tcpServer.bind((TCP_IP, TCP_PORT)) 
-threads = [] 
+# Server Tipo TCP
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Socket de Servidor iniciado.")
+#server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+serverSocket.bind((IP, PORT))
 
+threads = list()
 
-while True: 
-    tcpServer.listen(4) 
-    logging.info("Multithreaded Python server : Waiting for connections from TCP clients...")
-    print("Multithreaded Python server : Waiting for connections from TCP clients...")
-    (conn, (ip,port)) = tcpServer.accept() 
-    newthread = ClientThread(ip,port) 
-    newthread.start() 
-    threads.append(newthread) 
- 
-for t in threads: 
-    t.join() 
+# se aceptan 4 conexiones simultaneas
+serverSocket.listen(100)
+esperando = "Esperando clientes"
+logging.info(esperando)
+print(esperando)
+
+while True:
+	
+    (conn, (ip, port)) = serverSocket.accept()
+    
+    # se crea nuevo ServerThread 
+    newServerThread = ServerThread(ip, port)
+    newServerThread.start()
+    
+    # esperar hasta que cada serverThread termine
+    newServerThread.join()
+    threads.append(newServerThread)
+
+serverSocket.close()
